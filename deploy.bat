4e6d650a9cfb5f3880a5a0beb8b94a4caa1a1efd
kapsitis <kalvis.apsitis@gmail.com>
2020-03-30 02:00:14 +0300

    added deploy script fixes

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rem python "c:\Users\kalvis.apsitis\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py" c:\Users\kalvis.apsitis\workspace\linen-tracer-682
+python "c:\Users\kalvis.apsitis\AppData\Local\Google\Cloud SDK\google-cloud-sdk\platform\google_appengine\appcfg.py" update c:\Users\kalvis.apsitis\workspace\linen-tracer-682
+
+
+rem conda create -n py27 python=2.7 anaconda
+rem conda activate py27 
+rem conda deactivate
+
+rem https://docs.anaconda.com/anaconda/user-guide/tasks/switch-environment/
--- run-server.bat
+++ run-server.bat
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 python "c:\Users\kalvis.apsitis\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py" c:\Users\kalvis.apsitis\workspace\linen-tracer-682
-python "c:\Users\kalvis.apsitis\AppData\Local\Google\Cloud SDK\google-cloud-sdk\platform\google_appengine\appcfg.py" update c:\Users\kalvis.apsitis\workspace\linen-tracer-682
+rem python "c:\Users\kalvis.apsitis\AppData\Local\Google\Cloud SDK\google-cloud-sdk\platform\google_appengine\appcfg.py" update c:\Users\kalvis.apsitis\workspace\linen-tracer-682
 
 
 rem conda create -n py27 python=2.7 anaconda

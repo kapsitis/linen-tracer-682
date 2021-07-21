71905e352510d5b3ac2936762d784262504bef01
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2021-07-22 00:17:06 +0300

    Added sample flask

--- webflask/ddgatve/__init__.py
+++ webflask/ddgatve/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'ddgatve.sqlite'),
     )
 
     if test_config is None:
@@ -30,6 +30,13 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 

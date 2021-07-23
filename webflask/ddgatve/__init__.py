f9953285c8466a19132cc68c674b88aa654c8280
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2021-07-24 02:36:12 +0300

    Added Data Structures

--- webflask/ddgatve/__init__.py
+++ webflask/ddgatve/__init__.py
@@ -37,6 +37,9 @@ def create_app(test_config=None):
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
+    
+    from . import datastructures
+    app.register_blueprint(datastructures.bp)
 
     return app
 

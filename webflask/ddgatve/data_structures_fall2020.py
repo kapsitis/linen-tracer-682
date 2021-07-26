709809779b1034ebdee2a71d78505dc87dcf0f01
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2021-07-26 18:23:51 +0300

    data-structures use template inheritance

--- webflask/ddgatve/data_structures_fall2020.py
+++ webflask/ddgatve/data_structures_fall2020.py
@@ -9,45 +9,48 @@ from flask import (
 
 bp = Blueprint('data_structures_fall2020', __name__, url_prefix='/data-structures-fall2020')
 
+global_nav_items = None
+
+def getNavigItems(rootPath):
+    global global_nav_items
+    if global_nav_items == None:
+        navig_url = os.path.join(rootPath, 'static/data', 'global_navigation.json')
+        global_nav_items = json.load(open(navig_url))
+    return global_nav_items
+
+
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    navig_url = os.path.join(current_app.root_path, 'static/data', 'global_navigation.json')
-    nav_items = json.load(open(navig_url))
     json_url = os.path.join(current_app.root_path, 'static/data', 'data_structures_fall2020_topics.json')
     jsonTopics = json.load(open(json_url))
     template_context = {
-                'my_id': 'index.html',
-                'course': 'data-structures-fall2020',
-                'nav_items': nav_items,
-                'jsonTopics': jsonTopics
-            }
-    
+        'my_id': 'index',
+        'course': 'data-structures-fall2020',
+        'nav_items': getNavigItems(current_app.root_path),
+        'jsonTopics': jsonTopics
+    }
     return render_template('data_structures_fall2020/index.html', **template_context)
 
 
 @bp.route('/assignments', methods=['GET','POST'])
 def assignments():
-    navig_url = os.path.join(current_app.root_path, 'static/data', 'global_navigation.json')
-    nav_items = json.load(open(navig_url))
     template_context = {
-        'my_id': 'assignments.html',
+        'my_id': 'assignments',
         'course': 'data-structures-fall2020',
-        'nav_items': nav_items
+        'nav_items': getNavigItems(current_app.root_path)
     }
     return render_template('data_structures_fall2020/assignments.html', **template_context)
 
 
 @bp.route('/slides', methods=['GET','POST'])
 def slides():
-    navig_url = os.path.join(current_app.root_path, 'static/data', 'global_navigation.json')
-    nav_items = json.load(open(navig_url))
     modules_url = os.path.join(current_app.root_path, 'static/data', 'data_structures_fall2020_modules.json')
     jsonModules = json.load(open(modules_url))
     template_context = {
-        'my_id': 'slides.html',
+        'my_id': 'slides',
         'course': 'data-structures-fall2020',
         'jsonModules': jsonModules,
-        'nav_items': nav_items
+        'nav_items': getNavigItems(current_app.root_path)
     }
     
     return render_template('data_structures_fall2020/slides.html', **template_context)
@@ -55,13 +58,11 @@ def slides():
 
 @bp.route('/algorithms', methods=['GET','POST'])
 def algorithms():
-    navig_url = os.path.join(current_app.root_path, 'static/data', 'global_navigation.json')
-    nav_items = json.load(open(navig_url))
     template_context = {
-                'my_id': 'algorithms.html',
-                'course': 'data-structures-fall2020',
-                'nav_items': nav_items
-            }
+        'my_id': 'algorithms',
+        'course': 'data-structures-fall2020',
+        'nav_items': getNavigItems(current_app.root_path)
+    }
     
     return render_template('data_structures_fall2020/algorithms.html', **template_context)
 
@@ -69,16 +70,13 @@ def algorithms():
 
 @bp.route('/submissions', methods=['GET','POST'])
 def submissions():
-    navig_url = os.path.join(current_app.root_path, 'static/data', 'global_navigation.json')
-    nav_items = json.load(open(navig_url))
     grading_url = os.path.join(current_app.root_path, 'static/data', 'data_structures_fall2020_grading.json')
     gradingStuff = json.load(open(grading_url))
-
     template_context = {
-        'my_id': 'algorithms.html',
+        'my_id': 'submissions',
         'course': 'data-structures-fall2020',
         'gradingStuff': gradingStuff,
-        'nav_items': nav_items
+        'nav_items': getNavigItems(current_app.root_path)
     }
     return render_template('data_structures_fall2020/submissions.html', **template_context)
 

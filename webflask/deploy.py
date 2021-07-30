009794c1a21f436367207082bece6a1dd1349757
kapsitis <kalvis.apsitis@gmail.com>
2021-07-30 13:30:44 +0300

    Update deploy.py

--- webflask/deploy.py
+++ webflask/deploy.py
@@ -1,16 +1,31 @@
-from distutils.dir_util import copy_tree
-
-def copytree(src, dst, symlinks=False, ignore=None):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
+import os
+import os.path
+import shutil
 
 def main():
-    copy_tree('.', '/tmp/temp')
+    rootDir = os.path.abspath(".")
+    destRootDir = '/usr/local/webflask'
+    for root, dirs, files in os.walk(rootDir):
+        path = root.split(os.sep)
+        if len(path) < 6:
+            continue
+        if len(path) > 6 and path[6] in ['tests', 'venv', 'instance']:
+            continue
+        if len(path) > 7 and path[7] == '__pycache__':
+            continue
+        for file in files:
+            extension = os.path.splitext(file)[1].lower()
+            if extension in ['.cfg', '.in', '.ini', '.py', '.html'] or (len(path) > 8 and path[8] in ['reveal.js.40', 'algorithms-slides']):
+                srcpath = list(path)
+                srcpath.append(file)
+                #destpath = ['c:', 'Users', 'kapsitis', 'tmp']
+                destpath = destRootDir.split(os.sep)
+                destpath = destpath + srcpath[6:]
+
+                print('Copying ' + '/'.join(srcpath))
+                print('  to file ' + '/'.join(destpath))
+                os.makedirs(os.path.dirname('/'.join(destpath)), exist_ok=True)
+                shutil.copyfile('/'.join(srcpath), '/'.join(destpath))
 
 if __name__ == '__main__':
     main()

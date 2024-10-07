e7ec7780e9e7d85bf373db79a42fb3803adeedad
Kalvis Apsitis <kalvis.apsitis@zimperium.com>
2024-10-07 14:27:04 +0300

    aPulcins updated

--- webflask/ddgatve/aPulcins.py
+++ webflask/ddgatve/aPulcins.py
@@ -21,3 +21,15 @@ def index():
             }
     
     return render_template('aPulcins/index.html', **template_context)
+
+
+@bp.route('/worksheets', methods=['GET','POST'])
+def assignments():
+    navig_url = os.path.join(current_app.root_path, 'static/data', 'global_navigation.json')
+    nav_items = json.load(open(navig_url, encoding='utf-8'))
+    template_context = {
+        'my_id': 'worksheets',
+        'course': 'aPulcins',
+        'nav_items': nav_items
+    }
+    return render_template('aPulcins/worksheets.html', **template_context)
